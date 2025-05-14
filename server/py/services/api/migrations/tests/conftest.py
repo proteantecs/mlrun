55a2c379faced34adb1569e5a167f8460db58798
adamdelman <flyn@flyn.cc>
2025-05-14 10:35:14 +0300

    fix

--- server/py/services/api/migrations/tests/conftest.py
+++ server/py/services/api/migrations/tests/conftest.py
@@ -1,13 +1,28 @@
+# Copyright 2025 Iguazio
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 import pytest
 import sqlalchemy
 
 import mlrun
+
+
 @pytest.fixture(autouse=True)
 def truncate_all_tables():
     engine = sqlalchemy.create_engine(mlrun.mlconf.httpdb.dsn)
     with engine.connect() as conn:
         conn.exec_driver_sql("SET FOREIGN_KEY_CHECKS = 0")
-        for name, in conn.exec_driver_sql("SHOW TABLES"):
+        for (name,) in conn.exec_driver_sql("SHOW TABLES"):
             conn.exec_driver_sql(f"TRUNCATE TABLE `{name}`")
         conn.exec_driver_sql("SET FOREIGN_KEY_CHECKS = 1")
     yield

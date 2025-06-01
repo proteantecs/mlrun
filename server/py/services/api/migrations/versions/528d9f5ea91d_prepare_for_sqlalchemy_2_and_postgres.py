171fe9006f6d957b07b6262103b7aa4930b87092
adamdelman <flyn@flyn.cc>
2025-06-01 15:25:22 +0300

    fix

--- server/py/services/api/migrations/versions/528d9f5ea91d_prepare_for_sqlalchemy_2_and_postgres.py
+++ server/py/services/api/migrations/versions/528d9f5ea91d_prepare_for_sqlalchemy_2_and_postgres.py
@@ -15,7 +15,7 @@
 """Add missing on cascade delete to tag, tag_v2, artifact_tag and notification tables
 
 Revision ID: 528d9f5ea91d
-Revises: 6925effc8fb1
+Revises: b31651280cce
 Create Date: 2025-05-22 19:11:42.445053
 """
 
@@ -24,7 +24,7 @@ from sqlalchemy.dialects import mysql
 
 # revision identifiers, used by Alembic.
 revision = "528d9f5ea91d"
-down_revision = "6925effc8fb1"
+down_revision = "b31651280cce"
 branch_labels = None
 depends_on = None
 

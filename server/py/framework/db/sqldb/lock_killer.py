07f70e8d51420b04c81f8272af9de499c2fc7217
adamdelman <flyn@flyn.cc>
2025-06-09 21:02:33 +0300

    fix

--- server/py/framework/db/sqldb/lock_killer.py
+++ server/py/framework/db/sqldb/lock_killer.py
@@ -1,4 +1,17 @@
-from abc import ABC, abstractmethod
+# Copyright 2025 Iguazio
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import abc
 
 import sqlalchemy
 import sqlalchemy.exc
@@ -8,7 +21,7 @@ import mlrun
 import framework.db.sqldb.sql_session
 
 
-class LockKiller(ABC):
+class LockKiller(abc.ABC):
     dialect: None
 
     def __new__(cls, conn: sqlalchemy.Connection):
@@ -45,11 +58,11 @@ class LockKiller(ABC):
         pid, user, addr, obj = row
         return pid, user, addr, obj
 
-    @abstractmethod
+    @abc.abstractmethod
     def query(self) -> str:
         raise NotImplementedError()
 
-    @abstractmethod
+    @abc.abstractmethod
     def _terminate(self, pid: int) -> None:
         raise NotImplementedError()
 
